efbd126be0fbf6f77bad4dbef851e5d06ab342e7
im-kaushal <mail4kaushal.kr@gmail.com>
2023-02-15 21:56:38 +0530

    Contact Us

---
+++ src/screen/Contact.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+} from 'react-native';
+
+const ContactUs = () => {
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [message, setMessage] = React.useState('');
+
+  const handleNameChange = value => setName(value);
+  const handleEmailChange = value => setEmail(value);
+  const handleMessageChange = value => setMessage(value);
+
+  const handleSubmit = () => {
+    alert(
+      `Thank you ${name} for connecting with us! We'll get back to you soon :)`,
+    );
+  };
+  return (
+    <ImageBackground
+      source={{
+        uri: 'https://cdn.pixabay.com/photo/2022/01/09/07/32/presentation-6925464_1280.png',
+      }}
+      style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Contact Us</Text>
+      </View>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Name"
+          value={name}
+          onChangeText={handleNameChange}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          value={email}
+          onChangeText={handleEmailChange}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Message"
+          multiline={true}
+          numberOfLines={5}
+          value={message}
+          onChangeText={handleMessageChange}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+          <Text style={styles.buttonText}>Send Message</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  header: {
+    backgroundColor: '#8C7F69',
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  headerText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  form: {
+    width: '80%',
+    backgroundColor: '#8C7F69',
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  input: {
+    color: 'black',
+    width: '100%',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 5,
+    padding: 10,
+    marginVertical: 10,
+  },
+  button: {
+    backgroundColor: '#000',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default ContactUs;
